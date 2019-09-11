deb44cddd0d5175d71240ccd0cc8a7e074d95b31
denio-rus <denio@yandex.ru>
2019-09-11 19:23:19 +0700

    Add 500 status handler

--- app/services/equation_solver.rb
+++ app/services/equation_solver.rb
@@ -1,4 +1,6 @@
 class Services::EquationSolver
+  ERROR_MESSAGE_HASH = { 'error' => "Sorry, but our calculator is broken. Try it later." }.freeze
+
   def initialize(equation)
     @equation = equation
   end
@@ -10,8 +12,14 @@ class Services::EquationSolver
   def call
     res = HTTP.basic_auth(user: 'master', pass: "mathematics")
               .post('http://localhost:9292/solve_equation', body: @equation.to_json)
+    
+    return ERROR_MESSAGE_HASH if res.status.server_error?
+
     JSON.parse(res.body)
   rescue HTTP::ConnectionError => e
-    { 'error' => "Sorry, but our calculator is broken. Try it later." }
+    ERROR_MESSAGE_HASH
   end
+  
+  private
+
 end
